fca71cab44f3aa6c3358a141f7272dee73eed2f3
Abhilasha Purwar <abhilasha.purwar@gmail.com>
2024-04-11 10:48:09 +0530

    db full - input + display data

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,15 +1,16 @@
-"use client";
-
-//import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
 
-const OrganizationIdPage = () => {
-  //const { orgId } = auth();
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  const { orgId } = auth();
   return (
     <div className="w-full mb-20">
       <div className="px-2 md:px-4">
-        <p>Hi its me Organization : orgId</p>
+        <p>Hi its me Organization : {orgId}</p>
       </div>
       <form action={create}>
         <input
@@ -19,7 +20,15 @@ const OrganizationIdPage = () => {
           placeholder="Enter a board title"
           className="border-black border mt-4 p-2"
         />
+        <Button type="submit" className="mt-4">
+          Submit
+        </Button>
       </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>Board name: {board.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
